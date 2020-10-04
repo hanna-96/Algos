ebb5b4d76140a3075b5ded61ee40a1e0b6ac041b
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2020-10-04 13:51:26 -0400

    added a new solution to minstack

--- minStack.js
+++ minStack.js
@@ -30,16 +30,19 @@
   min [3,2]
  */
 
- //Time O(1)
+//Time O(1) Space O(n)
 var MinStack = function () {
   this.stack = [];
-  this.min = [];
+  this.min = []; //will help to keep track of min nums
 };
 
 MinStack.prototype.push = function (x) {
   if (this.stack.length === 0) {
     this.min.push(x);
   } else {
+    //everytime when we push a new num we need to find
+    // the a new minimum between the already excisting min num and a new num
+    //and push that new min to the minstack
     let minimum = Math.min(x, this.min[this.min.length - 1]);
     this.min.push(minimum);
   }
@@ -57,4 +60,52 @@ MinStack.prototype.top = function () {
 
 MinStack.prototype.getMin = function () {
   return this.min[this.min.length - 1];
+};
+//--------solution 2
+//time O(1); space O(n) the same as above
+var MinStack = function () {
+  this.stack = [];
+  this.minNums = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function (x) {
+  //we'll push a new num to minstack ONLY if it's lesser or equal to num
+  if (!this.stack.length || x <= this.minNums[this.minNums.length - 1])
+    this.minNums.push(x);
+  this.stack.push(x);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  //if a num is not the minimum from the minstack then fine we can easily delete it
+  if (
+    this.stack[this.stack.length - 1] !== this.minNums[this.minNums.length - 1]
+  ) {
+    this.stack.pop();
+  } else {
+    //but if it is the same as min then we pop it from both stacks
+    //so that a new min will be the one before it in the minstack
+    this.stack.pop();
+    this.minNums.pop();
+  }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.minNums[this.minNums.length - 1];
 };
