99d68a194905d85f8787e92c87738967128d458e
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2020-10-10 15:30:53 -0400

    added time/space complexity

--- isAnagram.js
+++ isAnagram.js
@@ -13,7 +13,14 @@
 
 // Follow up:
 // What if the inputs contain unicode characters? How would you adapt your solution to such case?
-
+//Answer
+// Use a hash table instead of a fixed size counter.
+//  Imagine allocating a large size array to fit the entire range of unicode characters, 
+//  which could go up to more than 1 million. 
+//  A hash table is a more generic solution and could adapt to any range of characters.
+//time O(n);
+//Space O(1)  Although we do use extra space, the space complexity is 
+// O(1) because the table's size stays constant no matter how large n is.
 var isAnagram = function (s, t) {
   let counter = 0;
   if (s.length !== t.length) return false;
@@ -30,7 +37,8 @@ var isAnagram = function (s, t) {
       return false;
     }
     //check if the count of the exising letter in obj exceeds the count of the current letters
-    //If we’re subtracting more specific letters from the first word than the first word even has, it means the second word has more letter “l”s than the first word, making it not an anagram.
+    //If we’re subtracting more specific letters from the first word than the first word even has, it means the second word 
+    //has more letter “l”s than the first word, making it not an anagram.
     if (dictionary[char] < 1) return false;
     dictionary[char]--;
   }
