e5c646948b3e9769b1c0e58fe6e48f026cee25a8
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2020-12-02 16:01:37 -0500

    added an optimized solution with O(1) space

--- palindromeLinkedList.js
+++ palindromeLinkedList.js
@@ -32,30 +32,40 @@ var isPalindrome = function (head) {
 //optimal solution  with O(1) space
 var isPalindrome = function (head) {
   if (head == null || head.next == null) return true;
+
+  // By representing the runners as pointers, and moving them down the list at the corresponding speeds, we can use this trick to find the middle of the list, and then split the list into two halves.
+  //1.split the list into 2 halves;
+  //2.reverse the 2nd part
+  //3.compare 2nd pard with the 1st
   let slow = head;
   let fast = head;
-  while (fast.next != null && fast.next.next != null) {
+
+  // By the time the fast runner gets to the end of the list, the slow runner will be half way.
+  while (fast != null && fast.next != null) {
     slow = slow.next;
     fast = fast.next.next;
   }
-  slow.next = reverseList(slow.next);
-  slow = slow.next;
+  //moving fast pointer back to the head
+  fast = head;
+  slow = reverseList(slow); //reversing the remaining second half
+  //where slow is the head of the second half
+
+  //slower half will be shorter
   while (slow != null) {
-    if (head.val != slow.val) return false;
-    head = head.next;
+    if (slow.val != fast.val) return false;
+    fast = fast.next;
     slow = slow.next;
   }
   return true;
 };
 
 var reverseList = function (head) {
-  let pre = null;
-  let next = null;
+  let prev = null;
   while (head != null) {
-    next = head.next;
-    head.next = pre;
-    pre = head;
-    head = next;
+    let nextNode = head.next;
+    head.next = prev;
+    prev = head;
+    head = nextNode;
   }
-  return pre;
+  return prev;
 };
