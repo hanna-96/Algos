cabc1c86484d74e019e61020cda91a7af8d46f3e
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2020-12-05 19:02:29 -0500

    added optimized solution

--- validParentheses.js
+++ validParentheses.js
@@ -31,58 +31,17 @@ var isValid = function (s) {
     //if we come across any closing bracket compare it to the top(last) element in the stack
     /// and If they are matching pairs->pop() from stack
     //continue till the stack is empty
-    else if (
-      (stack.length && bracket === ")") ||
-      (stack.length && bracket === "]") ||
-      (stack.length && bracket === "}")
-    ) {
+    else if (bracket === ")" || bracket === "]" || bracket === "}") {
+      if (!stack.length) return false;
       if (memo[bracket] === stack[stack.length - 1]) {
         stack.pop();
       } else {
         return false;
       }
-    } else {
-      return false;
     }
   }
   //if the stavk is empty a->then we found all the mathcing pairs->So they are valid(return true)
   return !stack.length;
 };
-// Time complexity : O(n)O(n) because we simply traverse the given string one character at a time and push and pop operations on a stack take O(1)O(1) time.
-// Space complexity : O(n)O(n) as we push all opening brackets onto the stack and in the worst case, we will end up pushing all the brackets onto the stack. e.g. ((((((((((.
-
-//solution 2
-var isValid = function (s) {
-  let memo = {
-    ")": "(",
-    "}": "{",
-    "]": "[",
-  };
-  let stack = [];
-  for (let bracket of s) {
-    if (bracket === "(" || bracket === "[" || bracket === "{") {
-      stack.push(bracket);
-    } else if (
-      stack.length &&
-      bracket === ")" &&
-      stack[stack.length - 1] === "("
-    ) {
-      stack.pop();
-    } else if (
-      stack.length &&
-      bracket === "]" &&
-      stack[stack.length - 1] === "["
-    ) {
-      stack.pop();
-    } else if (
-      stack.length &&
-      bracket === "}" &&
-      stack[stack.length - 1] === "{"
-    ) {
-      stack.pop();
-    } else {
-      return false;
-    }
-  }
-  return !stack.length;
-};
+// Time complexity : O(n) because we simply traverse the given string one character at a time and push and pop operations on a stack take O(1)O(1) time.
+// Space complexity : O(n) as we push all opening brackets onto the stack and in the worst case, we will end up pushing all the brackets onto the stack. e.g. ((((((((((.
