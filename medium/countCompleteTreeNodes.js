3a57b2053e4a97fcc41b46fd3de0131ec66ef522
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2020-12-29 10:51:08 -0500

    added a more oprimal solution

--- medium/countCompleteTreeNodes.js
+++ medium/countCompleteTreeNodes.js
@@ -21,6 +21,34 @@
 // Space O(d) = O(log n) to keep the recursion stack, where d is a tree depth
 var countNodes = function (root) {
   if (!root) return 0;
-  // if(root.left === null && root.right ===null) return 1;
+  return countNodes(root.left) + countNodes(root.right) + 1;
+};
+//solution 2 (covers a case of a perfect complete tree)
+//if it's perfect tree ->Time: O(h),where h is height
+// if not a perfect Time:O(n)
+var countNodes = function (root) {
+  //case 1 :check if it is a perfect binary tree(all levels are fully completed) and
+  // number of nodes are = 2^h -1
+  let leftHeight = 0;
+  let rightHeight = 0;
+  let pRight = root;
+  let pLeft = root;
+
+  //now using pointers let's get the number of nodes to root's left
+
+  while (pLeft !== null) {
+    leftHeight++;
+    pLeft = pLeft.left;
+  }
+  //how many nodes in the right subtree
+  while (pRight !== null) {
+    rightHeight++;
+    pRight = pRight.right;
+  }
+
+  //check if this complete binary tree is perfect
+  if (leftHeight === rightHeight) return Math.pow(2, leftHeight) - 1;
+
+  //case 2:not perfect
   return countNodes(root.left) + countNodes(root.right) + 1;
 };
