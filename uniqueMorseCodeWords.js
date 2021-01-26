8ddbeccc8063bb6f35964ec20bd01766904a7158
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-01-26 11:50:15 -0500

    unique morse code words

---
+++ uniqueMorseCodeWords.js
@@ -0,0 +1,66 @@
+// International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+// For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+// [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+// Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-..--...", (which is the concatenation "-.-." + ".-" + "-..."). We'll call such a concatenation, the transformation of a word.
+
+// Return the number of different transformations among all words we have.
+
+// Example:
+// Input: words = ["gin", "zen", "gig", "msg"]
+// Output: 2
+// Explanation:
+// The transformation of each word is:
+// "gin" -> "--...-."
+// "zen" -> "--...-."
+// "gig" -> "--...--."
+// "msg" -> "--...--."
+
+// There are 2 different transformations, "--...-." and "--...--.".
+
+const uniqueMorseRepresentations = (words) => {
+  let set = new Set();
+  let morseCode = [
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
+  ];
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    let transformedVersion = "";
+    for (let j = 0; j < word.length; j++) {
+      let index = alphabet.indexOf(word[j]);
+      let char = morseCode[index];
+      transformedVersion += char;
+    }
+    set.add(transformedVersion);
+  }
+
+  return set.size;
+};
