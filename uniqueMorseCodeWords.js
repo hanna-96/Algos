8eca67421abf8d9c536a3329d0f8e0ed4e2088ea
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-01-26 12:06:46 -0500

    added another solution using charCodeAt()

--- uniqueMorseCodeWords.js
+++ uniqueMorseCodeWords.js
@@ -64,3 +64,48 @@ const uniqueMorseRepresentations = (words) => {
 
   return set.size;
 };
+//option 2 using charCodeAt()
+const uniqueMorseRepresentations = (words) => {
+  var morse = [
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
+  ];
+
+  var transformations = new Set();
+
+  for (let word of words) {
+    var trans = "";
+    for (let letter of word) {
+      var index = letter.charCodeAt(0) - 97;
+      trans += morse[index];
+    }
+
+    transformations.add(trans);
+  }
+
+  return transformations.size;
+};
