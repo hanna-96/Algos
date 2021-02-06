a0b1dc5f103611523f70ca90f0258b4e40c1884b
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-02-05 19:45:15 -0500

    added a more optimal solution

--- medium/reorganizeString.js
+++ medium/reorganizeString.js
@@ -14,7 +14,6 @@
 
 // S will consist of lowercase letters and have length in range [1, 500].
 
-
 var reorganizeString = function (S) {
   let arr = S.split("");
   //count how many times each character occur
@@ -44,4 +43,33 @@ var reorganizeString = function (S) {
     }
   }
   return res.join("");
+};
+//solution 2 using priority queue
+var reorganizeString = function (S) {
+  let hash = {},
+    pq = [];
+  for (let s of S) hash[s] = hash[s] + 1 || 1;
+
+  for (let key in hash) pq.push([key, hash[key]]);
+
+  pq.sort((a, b) => b[1] - a[1]);
+
+  let res = "";
+  while (pq.length != 0) {
+    let lastChar = res[res.length - 1];
+    let first = pq.shift();
+
+    if (lastChar != first[0]) {
+      res += first[0];
+      if (first[1] != 1) pq.push([first[0], first[1] - 1]);
+    } else {
+      let second = pq.shift();
+      if (second == null) return "";
+      res += second[0];
+      pq.push(first);
+      if (second[1] != 1) pq.push([second[0], second[1] - 1]);
+    }
+    pq.sort((a, b) => b[1] - a[1]);
+  }
+  return res;
 };
