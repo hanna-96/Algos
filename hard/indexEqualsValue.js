1f110eef55836eea0e58d42a0078feebf7f5b4ae
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-02-14 20:21:22 -0500

    added a more optimal solution

--- hard/indexEqualsValue.js
+++ hard/indexEqualsValue.js
@@ -13,3 +13,33 @@ function indexEqualsValue(array) {
   }
   return -1;
 }
+
+//optimal Solution
+//Time O(log n); Space O(1)
+function indexEqualsValue(array) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2); //2
+    if (array[mid] < mid) {
+      //all values to its left will be smaller then its corresponding indeces,because
+      //it's a sorted array, so all indeces will decrease by at least 1. and all values will decrease by at least 1
+      left = mid + 1;
+    } else if (array[mid] === mid && mid === 0) {
+      // !!!meaning we found the 0th index in the array that === to its value and there're no more elements before this one
+      return mid;
+    } else if (array[mid] === mid && array[mid - 1] < mid - 1) {
+      //if we found the first match =>We still need to find if there are the elemnts before that might also be a match
+      //BUT if the leftmost neighbor of current element is < than the index of that neighbor=>There will not
+      //be any matches on the leftside(because all values will be less then its indeces), so return the current mid!
+      return mid;
+    } else {
+      //If the current value is > i , than ALL the values to its right will be > than indeces,so explore left side
+      // OR  if the leftmost neighbor of current element is === to the index of that neighbor
+      //There will be definitely another match on the left side, so explore it!
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
