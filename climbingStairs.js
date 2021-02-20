bcb60c98b64729517dbb5fc382a480c4a38014c2
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-02-19 21:45:35 -0500

    added more optimal solution

--- climbingStairs.js
+++ climbingStairs.js
@@ -36,3 +36,16 @@ const climbStairs = (n) => {
   }
   return diffWays[n];
 };
+//Time O(n), space O(1)
+const climbStairs = (n) => {
+  if (n === 1) return 1;
+  let first = 1;
+  let second = 2;
+  let third;
+  for (let i = 3; i <= n; i++) {
+    third = first + second;
+    first = second;
+    second = third;
+  }
+  return second;
+};
