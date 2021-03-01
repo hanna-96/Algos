56bc711983b86b2b9fcbb7ebbd149204a63b8c9e
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-03-01 13:17:36 -0500

    solved withous using stack

--- medium/countAndSay.js
+++ medium/countAndSay.js
@@ -53,4 +53,24 @@ const countAndSay = (n) => {
       return helper(n - 1, copy);
     }
   }
+
+  //solution 2 without using a stack
+  const countAndSay = (n, str = '1') => {
+    if (n === 1) {
+      return str;
+    }
+    let newStr = '',
+      count = 0,
+      say = str[0];
+    for (let i = 0; i < str.length; i += 1) {
+      if (str[i] === say) {
+        count += 1;
+      } else {
+        newStr += count + say;
+        count = 1;
+        say = str[i];
+      }
+    }
+    return countAndSay(n - 1, newStr + count + say);
+  };
   
