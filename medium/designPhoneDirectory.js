de9948dbdecd8d180c74019d9807d6e4905839b3
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-03-05 19:04:42 -0500

    design phone directory

---
+++ medium/designPhoneDirectory.js
@@ -0,0 +1,61 @@
+// Design a Phone Directory which supports the following operations:
+
+// get: Provide a number which is not assigned to anyone.
+// check: Check if a number is available or not.
+// release: Recycle or release a number.
+
+// Example:
+
+// // Init a phone directory containing a total of 3 numbers: 0, 1, and 2.
+// PhoneDirectory directory = new PhoneDirectory(3);
+
+// // It can return any available phone number. Here we assume it returns 0.
+// directory.get();
+
+// // Assume it returns 1.
+// directory.get();
+
+// // The number 2 is available, so return true.
+// directory.check(2);
+
+// // It returns 2, the only number that is left.
+// directory.get();
+
+// // The number 2 is no longer available, so return false.
+// directory.check(2);
+
+// // Release number 2 back to the pool.
+// directory.release(2);
+
+// // Number 2 is available again, return true.
+// directory.check(2);
+
+//solution 1 , using queue
+// maxNumbers - The maximum numbers that can be stored in the phone directory.
+//Time O(n), Space O(n)
+var PhoneDirectory = function (maxNumbers) {
+  this.queue = [];
+  for (let i = 0; i < maxNumbers; i++) {
+    this.queue.push(i);
+  }
+};
+
+//  Provide a number which is not assigned to anyone.
+//  Return an available number. Return -1 if none is available.
+//Time O(1), Space O(1)
+PhoneDirectory.prototype.get = function () {
+  if (!this.queue.length) return -1;
+  return this.queue.shift();
+};
+
+//  Check if a number is available or not.
+//Time O(n), Space O(1)
+PhoneDirectory.prototype.check = function (number) {
+  return this.queue.includes(number);
+};
+
+//  Recycle or release a number.
+//Time O(n), Space O(1)
+PhoneDirectory.prototype.release = function (number) {
+  if (!this.queue.includes(number)) this.queue.push(number);
+};
