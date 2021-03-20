404da77e8ce31bc90e82e4d94c58ef2457a7d6fa
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-03-19 21:12:46 -0400

    merge k sorted lists

---
+++ hard/mergeKSortedLists.js
@@ -0,0 +1,49 @@
+// You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+// Merge all the linked-lists into one sorted linked-list and return it.
+
+// Example 1:
+
+// Input: lists = [[1,4,5],[1,3,4],[2,6]]
+// Output: [1,1,2,3,4,4,5,6]
+// Explanation: The linked-lists are:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// merging them into one sorted list:
+// 1->1->2->3->4->4->5->6
+// Example 2:
+
+// Input: lists = []
+// Output: []
+//approach 1, using extra space(creating a new LL)
+//Time O(k*N), where k is number od linked lists, n is the total number of nodes in each list
+//Space O(k)
+var mergeKLists = function (lists) {
+  if (!lists.length) return null;
+  //every time compare two LL(starting from first two LL) and merge them into one LL
+
+  let mergedList = lists[0];
+
+  for (let i = 1; i < lists.length; i++) {
+    let currentLL = lists[i];
+    mergedList = mergeTwoSortedLists(mergedList, currentLL);
+  }
+  return mergedList;
+};
+//1-4-5 && 1-3-4
+function mergeTwoSortedLists(l1, l2) {
+  let p1 = l1;
+  let p2 = l2;
+  if (!p1) return p2;
+  if (!p2) return p1;
+
+  if (p1.val <= p2.val) {
+    p1.next = mergeTwoSortedLists(p1.next, p2);
+    return p1;
+  } else {
+    p2.next = mergeTwoSortedLists(p1, p2.next);
+    return p2;
+  }
+}
