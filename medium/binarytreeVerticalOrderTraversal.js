1d34dc75a9aa5f7d1c9dae3473b778f01d0d10f9
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-03-21 14:00:27 -0400

    traverse binary tree in vertical order

---
+++ medium/binarytreeVerticalOrderTraversal.js
@@ -0,0 +1,48 @@
+// Given the root of a binary tree, return the vertical order traversal of its nodes' values. (i.e., from top to bottom, column by column).
+
+// If two nodes are in the same row and column, the order should be from left to right.
+
+ 
+
+// Example 1:
+//     3
+//     / \
+//    9   20
+//        / \
+//        15  7
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[9],[3,15],[20],[7]]
+
+//Time O(n log n), because of sorting;
+//Space O(n), we store n nodes in a hash map and we store all n nodes in output array
+var verticalOrder = function (root) {
+    let output = [];
+    let queue = [[root, 0]];
+    let map = new Map();
+  
+    if (!root) return output;
+  
+    while (queue.length) {
+      let [currentNode, col] = queue.shift();
+      if (currentNode !== null) {
+        if (!map.has(col)) map.set(col, [currentNode.val]);
+        else {
+          let nodes = map.get(col);
+          nodes.push(currentNode.val);
+        }
+        queue.push([currentNode.left, col - 1]);
+        queue.push([currentNode.right, col + 1]);
+      }
+    }
+    let nodesFromMap = [];
+    for (let [col, nodes] of map.entries()) {
+      nodesFromMap.push([col, nodes]);
+    }
+    nodesFromMap.sort((a, b) => a[0] - b[0]);
+    for (let [col, nodes] of nodesFromMap) {
+      output.push(nodes);
+    }
+    return output;
+  };
+  
