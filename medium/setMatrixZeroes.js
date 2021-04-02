73a3581b22869a9fe70b69ade04ef72b52e81603
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-04-02 13:36:54 -0400

    solved in constant space

--- medium/setMatrixZeroes.js
+++ medium/setMatrixZeroes.js
@@ -37,3 +37,55 @@ function nullifyCol(matrix, col) {
     matrix[row][col] = 0;
   }
 }
+
+
+imal space solution
+//Time O(M * N); Space O(1)
+var setZeroes = function (matrix) {
+  /*
+        1) iterate over the matrix, if the value is 0 then mark  the first cell of a row  matrix[i][0] to be 0 and
+         the first cell of a column matrix[0][j] to be 0.  Later If the first cell of a row is set to zero this means the row should be marked zero. 
+        Or If the first cell of a column is set to zero this means the column should be marked zero
+        2) iterate over the matrix again but this time starting from i = 1 and j = 1,
+        we do this, so we won't lose reference to the col and rows that have to be
+        change to 0
+        3) we have one more edge case, we need to check if the first row and col
+        were initially 0 before we set them up to be, if so change the whole row or col
+        to 0
+    */
+  let n = matrix.length;
+  let c = matrix[0].length;
+  let firstCol = false; // this flag would determine whether column has been set to zero.
+
+  for (let i = 0; i < n; i++) {
+    if (matrix[i][0] === 0) firstCol = true;
+    for (let j = 0; j < c; j++) {
+      // If an element is zero, we set the first element of the corresponding row and column to 0
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0; // mark the current row(later all the cells will be set to 0 s)
+        matrix[0][j] = 0; // mark the current col(later all the cells will be set to 0 s)
+      }
+    }
+  }
+  //   Iterate over the array once again and using the first row and first column, update the elements.
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < c; j++) {
+      // check if we marked current row or col before
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+  // check if the first row needs to be set to zero as well
+  if (matrix[0][0] === 0) {
+    for (let i = 0; i < c; i++) {
+      matrix[0][i] = 0;
+    }
+  }
+  // check if the first column needs to be set to zero as well
+  if (firstCol) {
+    for (let i = 0; i < n; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+};
