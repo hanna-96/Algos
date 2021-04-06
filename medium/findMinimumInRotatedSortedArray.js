e2eb675c8262ef7db41a0268bbccd48573455886
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-04-05 22:30:51 -0400

    added somw comments

--- medium/findMinimumInRotatedSortedArray.js
+++ medium/findMinimumInRotatedSortedArray.js
@@ -1,9 +1,8 @@
-//Time O(log n); Space O(log n)
+//Time O(log n); Space O(log n); Binary search
 var findMin = function (nums) {
   if (nums.length < 2) return nums[0];
   let left = 0;
   let right = nums.length - 1;
-
   // if the array is not rotated
   if (nums[right] > nums[0]) return nums[0];
 
@@ -16,7 +15,7 @@ var findMin = function (nums) {
     // if the mid element is lesser than its previous element then mid element is the smallest
 
     if (nums[mid - 1] > nums[mid]) return nums[mid];
-    //   if the mid elements value is greater than the 0th element this means
+    //  if the mid elements value is greater than the 0th element this means
     // the least value is still somewhere to the right as we are still dealing with elements greater than nums[0]
     if (nums[mid] > nums[0]) {
       left = mid + 1;
