153cd5e26ba7331eb85bbef75915719bbf8a130c
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-04-08 16:59:29 -0400

    solved using binary search

--- findIndexInMountainArray.js
+++ findIndexInMountainArray.js
@@ -1,6 +1,24 @@
 // Time O(n); Space O(1)
-var peakIndexInMountainArray = function(arr) {
-    for(let i = 1; i< arr.length; i++){
-        if(arr[i] > arr[i-1] && arr[i] > arr[i+1]) return i;
-    }
+var peakIndexInMountainArray = function (arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) return i;
+  }
 };
+
+// optimal solution Time (log n),Space O(1)
+var peakIndexInMountainArray = function (arr) {
+  return helper(arr, 0, arr.length - 1);
+};
+function helper(arr, left, right) {
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) return mid;
+    if (arr[mid] > arr[mid + 1]) {
+      // discover left side
+      right = mid - 1;
+    } else if (arr[mid] <= arr[mid + 1]) {
+      // explore right side
+      left = mid + 1;
+    }
+  }
+}
