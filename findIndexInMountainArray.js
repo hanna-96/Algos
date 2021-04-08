bcc2d88d13295da6afc5fb8fffa93b7574d56d23
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-04-08 16:58:41 -0400

    find index in mountain array

---
+++ findIndexInMountainArray.js
@@ -0,0 +1,6 @@
+// Time O(n); Space O(1)
+var peakIndexInMountainArray = function(arr) {
+    for(let i = 1; i< arr.length; i++){
+        if(arr[i] > arr[i-1] && arr[i] > arr[i+1]) return i;
+    }
+};
