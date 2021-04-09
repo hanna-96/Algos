5b69e8530824158b37fbfbdbe1fa5ee0e6090fe3
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-04-09 15:48:46 -0400

    solved using binary search

--- findPeakIndexInMountainArray.js
+++ findPeakIndexInMountainArray.js
@@ -5,3 +5,24 @@ var peakIndexInMountainArray = function (arr) {
     if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) return i;
   }
 };
+
+
+  //Binary search solution
+  // Time O(log n);Space O(1)
+  var peakIndexInMountainArray = function (arr) {
+    return helper(arr, 0, arr.length - 1);
+  };
+  function helper(arr, left, right) {
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) return mid;
+      if (arr[mid] > arr[mid + 1]) {
+        // discover left side as the peak shoud be earlier( we are going down further, so no need to explore right side)
+        right = mid - 1;
+      } else if (arr[mid] <= arr[mid + 1]) {
+        // explore right side, because we haven't  reached the peak yet(we are going up the moantain still)
+        left = mid + 1;
+      }
+    }
+  }
+  
