8fcf95a17e5fd24a2120dd080dbdeddc9d47384f
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-04-08 20:39:32 -0400

    solved using binary heap

--- medium/meetingRooms2.js
+++ medium/meetingRooms2.js
@@ -72,54 +72,88 @@ var minMeetingRooms = function (intervals) {
   }
   return max;
 };
-
 // solution 3 using heap
-const minMeetingRooms = (intervals) => {
-  const   compareFunc = (a, b) => a - b,
-          minHeap = new MinHeap(compareFunc);
-  
-  intervals.sort((a, b) => a[0] - b[0]);
-  
-  let maxRooms = 0;
-  
-  intervals.forEach(interval => {
-      if(minHeap.size > 0 && minHeap.peek() <= interval[0]) {
-          minHeap.extract();
-      }
-      
-      minHeap.insert(interval[1]);
-      
-      maxRooms = Math.max(maxRooms, minHeap.size);
-  });
-  
-  return maxRooms;
+const minMeetingRooms = (times) => {
+  if (times.length < 1) return 0;
+  times.sort((a, b) => a[0] - b[0]);
+
+  let laptops = [times[0]];
+  let heap = new MinHeap(laptops);
+
+  for (let i = 1; i < times.length; i++) {
+    let currentInterval = times[i];
+    if (currentInterval[0] >= heap.peek()[1]) heap.remove();
+    heap.insert(currentInterval);
+  }
+
+  return laptops.length;
 };
 
-// since JS does not have a native heap, 
-// for an interview you can quickly code-up something like this
-// letting interviewer know what you are doing
 class MinHeap {
-  constructor(compareFunc) {
-      this.compareFunc = compareFunc;
-      this.heap = [];
+  constructor(array) {
+    this.heap = this.buildHeap(array);
   }
-  
-  insert(val) {
-      this.heap.unshift(val);
-      this.heap.sort(this.compareFunc);
+  // O( n ) time , O(1) space
+  buildHeap(array) {
+    let parentIdx = Math.floor((array.length - 1 - 1) / 2);
+    for (let currentIdx = parentIdx; currentIdx >= 0; currentIdx--) {
+      this.siftDown(currentIdx, array.length - 1, array);
+    }
+    return array;
   }
-  
-  extract() {
-      if (this.size === 0) return null;
-      return this.heap.shift();
+  // O(log n ) time , O(1) space
+  // putting every element before parent to the right position in the heap
+  siftDown(currentIdx, endIdx, heap) {
+    let leftChildIdx = 2 * currentIdx + 1;
+    while (leftChildIdx <= endIdx) {
+      let rightChildIdx =
+        currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+      let idxToSwap;
+      if (
+        rightChildIdx !== -1 &&
+        heap[rightChildIdx][1] < heap[leftChildIdx][1]
+      ) {
+        idxToSwap = rightChildIdx;
+      } else {
+        idxToSwap = leftChildIdx;
+      }
+      if (heap[idxToSwap][1] < heap[currentIdx][1]) {
+        this.swap(currentIdx, idxToSwap, heap);
+        currentIdx = idxToSwap;
+        leftChildIdx = currentIdx * 2 + 1;
+      } else {
+        return;
+      }
+    }
   }
-  
+  // O(log n ) time , O(1) space
+  siftUp(currentIdx, heap) {
+    let parentIdx = Math.floor((currentIdx - 1) / 2);
+    while (currentIdx > 0 && heap[currentIdx][1] < heap[parentIdx][1]) {
+      this.swap(currentIdx, parentIdx, heap);
+      currentIdx = parentIdx;
+      parentIdx = Math.floor((currentIdx - 1) / 2);
+    }
+  }
+  // O(1) time , O(1) space
   peek() {
-      if(this.size === 0) return null;
-      return this.heap[0];
+    return this.heap[0];
   }
-  
-  get size() {
-      return this.heap.length;
+  // O(log n ) time , O(1) space
+  remove() {
+    this.swap(0, this.heap.length - 1, this.heap);
+    const valueToRemove = this.heap.pop();
+    this.siftDown(0, this.heap.length - 1, this.heap);
+    return valueToRemove;
+  }
+  // O(log n ) time , O(1) space
+  insert(value) {
+    this.heap.push(value);
+    this.siftUp(this.heap.length - 1, this.heap);
+  }
+  swap(start, end, array) {
+    let temp = array[start];
+    array[start] = array[end];
+    array[end] = temp;
   }
 }
