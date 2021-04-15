b471d019d7c03eca6288b588c4f57db8109cf375
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2021-04-15 12:59:57 -0400

    solved using max heap

--- medium/kthLargestElementInArray.js
+++ medium/kthLargestElementInArray.js
@@ -44,3 +44,59 @@ function swap(left, right, array) {
   array[left] = array[right];
   array[right] = swap;
 }
+/* Solution using MaxHeap
+Build Heap Time O(n),Space O(1);
+SiftDown O(log n)
+
+ */
+var findKthLargest = function (nums, k) {
+  if (nums.length === 1) return k;
+  let res;
+  let heap = new MaxHeap(nums);
+  while (k > 0) {
+    res = heap.remove();
+    k--;
+  }
+  return res;
+};
+class MaxHeap {
+  constructor(arr) {
+    this.heap = this.buildHeap(arr);
+  }
+  buildHeap(arr) {
+    let parentIdx = Math.floor((arr.length - 1 - 1) / 2);
+    for (let currentIdx = parentIdx; currentIdx >= 0; currentIdx--) {
+      this.siftDown(currentIdx, arr.length - 1, arr);
+    }
+    return arr;
+  }
+  siftDown(currentIdx, endIdx, arr) {
+    let leftChildIdx = currentIdx * 2 + 1;
+    while (leftChildIdx <= endIdx) {
+      let rightChildIdx =
+        currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+      let idxToSwap;
+      if (rightChildIdx !== -1 && arr[rightChildIdx] > arr[leftChildIdx])
+        idxToSwap = rightChildIdx;
+      else idxToSwap = leftChildIdx;
+
+      if (arr[idxToSwap] > arr[currentIdx]) {
+        this.swap(currentIdx, idxToSwap, arr);
+        currentIdx = idxToSwap;
+        leftChildIdx = currentIdx * 2 + 1;
+      } else return;
+    }
+  }
+
+  remove() {
+    this.swap(0, this.heap.length - 1, this.heap);
+    let elToRemove = this.heap.pop();
+    this.siftDown(0, this.heap.length - 1, this.heap);
+    return elToRemove;
+  }
+  swap(i, j, arr) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+}
